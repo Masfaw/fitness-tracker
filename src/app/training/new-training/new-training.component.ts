24cfecb2ae30c1322faa638c376451d8fa827a6c
Matheos Asfaw <matheos.asfaw@gmail.com>
2018-05-21 01:08:02 -0700

    fetch metadata and data from firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -22,7 +23,17 @@ export class NewTrainingComponent implements OnInit {
   ngOnInit() {
     this.exercises = this.db
       .collection('availableExercises')
-      .valueChanges();
+      .snapshotChanges().map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            // ...doc.payload.doc.data()
+            name: doc.payload.doc.data().name,
+            duration: doc.payload.doc.data().duration,
+            calories: doc.payload.doc.data().calories,
+          };
+        });
+    });
   }
 
   onStartTraining(form: NgForm) {
